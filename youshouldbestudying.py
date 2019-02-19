68a45a14034b55bee2a8b5debd945d1d776a76b7
bngr <45574053+bngr@users.noreply.github.com>
2019-02-19 11:32:55 -0600

    Update youshouldbestudying.py

--- youshouldbestudying.py
+++ youshouldbestudying.py
@@ -1,11 +1,12 @@
+#check if i'm playing apex legends instead of studying and then minimize it
 import win32ui
 import win32gui
 import time
-
 
 localtime = time.asctime(time.localtime(time.time())
 def areYouGaming(localtime):
     if FindWindow("Apex Legends", "Apex Legends"):
         print "Apex Legends found at ", localtime
+        time.sleep(200)
         Minimize = win32gui.GetForegroundWindow()
         win32gui.ShowWindow(Minize, win32con.SW_MINIMIZE)
