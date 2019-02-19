cf875382ba3d13c0e4d7af661b5bfa4c81345425
bngr <45574053+bngr@users.noreply.github.com>
2019-02-19 11:38:20 -0600

    Update youshouldbestudying.py

--- youshouldbestudying.py
+++ youshouldbestudying.py
@@ -1,12 +1,23 @@
-#check if i'm playing apex legends instead of studying and then minimize it
+#check if i'm playing apex legends instead of studying
+#minimize the Game
+#play an annoying ass sound
+#test on home PC
+
 import win32ui
 import win32gui
 import time
+import winsound
 
 localtime = time.asctime(time.localtime(time.time())
+
+#download some annoying sound and place it in script dir as alert.wav
 def areYouGaming(localtime):
     if FindWindow("Apex Legends", "Apex Legends"):
         print "Apex Legends found at ", localtime
         time.sleep(200)
         Minimize = win32gui.GetForegroundWindow()
         win32gui.ShowWindow(Minize, win32con.SW_MINIMIZE)
+        windsound.Playsound('alert.wav', winsound.SND_FILENAME)
+        
+while True:
+    areYouGaming()
