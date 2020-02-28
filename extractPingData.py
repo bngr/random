b0c4df65cc24b4d006637ad44d8f01091d1160d2
bngr <45574053+bngr@users.noreply.github.com>
2020-02-28 13:33:17 -0600

    Create extractPingData.py

---
+++ extractPingData.py
@@ -0,0 +1,18 @@
+from scapy.all import *
+import sys
+import base64
+
+# script to extract data from ping padding (http://wumb0.in/ping-exfil.html)
+
+try:
+    config.conf.iface = sys.argv[2]
+except: pass
+
+s = sniff(lfilter=lambda x: x.haslayer(ICMP) and x[ICMP].type==8, stop_filter=lambda x: x.haslayer(ICMP) and x[ICMP].type==8 and '\n' in x[ICMP][Raw].load[8:24])
+
+buf = ""
+for i in s:
+    buf += i[ICMP][Raw].load[8:24]
+
+with open(sys.argv[1], "w") as f:
+    f.write(base64.b64decode(buf[:buf.find('\n')]))
